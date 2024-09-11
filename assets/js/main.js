4424ae12c4d5c05c946817a142006bd411719cf8
JaejunL <jjlee0721@gmail.com>
2024-09-11 17:21:01 +0900

    240911

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Photon by HTML5 UP
+	Telephasic by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,12 +11,11 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
+			normal:    [ '1081px',  '1280px'  ],
+			narrow:    [ '821px',   '1080px'  ],
+			narrower:  [ '737px',   '820px'   ],
+			mobile:    [ '481px',   '736px'   ],
+			mobilep:   [ null,      '480px'   ]
 		});
 
 	// Play initial animations on page load.
@@ -26,7 +25,42 @@
 			}, 100);
 		});
 
-	// Scrolly.
-		$('.scrolly').scrolly();
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			speed: 300,
+			alignment: 'center',
+			noOpenerFade: true
+		});
+
+	// Nav.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						'<a href="index.html" class="link depth-0">Home</a>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'top',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
 })(jQuery);
